fb52435b1feed6f22064a908777b41c85a9342bf
stefan <stefanbvts@gmail.com>
2024-07-07 16:57:56 +0200

    restore company option

--- bin/build.sh
+++ bin/build.sh
@@ -5,6 +5,8 @@ start=$(date +%s)
 source "$(dirname "$0")/build-vars.sh"
 DEST_DIR='build'
 
+cd ../
+
 mkdir -p "$DEST_DIR"
 cp -rpv app public src templates .env .htaccess composer.json composer.lock firebase.json "$DEST_DIR/"
 cd "$DEST_DIR/"
@@ -16,7 +18,10 @@ sed -i "s/^${ENV_DB_USER}=.*/${ENV_DB_USER}='$(printf '%s\n' "$NEW_DB_USER" | se
 sed -i "s/^${ENV_DB_PASSWD}=.*/${ENV_DB_PASSWD}='$(printf '%s\n' "$NEW_DB_PASSWD" | sed 's/[\/&]/\\&/g')'/" ./.env
 sed -i "s/^${ENV_URL_FE}=.*/${ENV_URL_FE}='$(printf '%s\n' "$NEW_URL_FE" | sed 's/[\/&]/\\&/g')'/" ./.env
 sed -i "s/^${ENV_URL_BE}=.*/${ENV_URL_BE}='$(printf '%s\n' "$NEW_URL_BE" | sed 's/[\/&]/\\&/g')'/" ./.env
-sed -i "s|window.open('http://localhost:3000', '_blank');|window.open('${NEW_URL_FE}', '_blank');|g" ./templates/pages/welcome_screen.html
+sed -i "s|let baseURLBE = \"http://www.insystem-api.localhost\";|let baseURLBE = \"${NEW_URL_BE}\";|g" ./templates/pages/welcome_screen.html
+sed -i "s|let URL_FE = \"https://localhost:3000\";|let URL_FE = \"${NEW_URL_FE}\";|g" ./templates/pages/welcome_screen.html
+sed -i "s|iFrame.setAttribute(\"src\", baseURLBE + currentPath);|iFrame.setAttribute(\"src\", baseURLBE + \"/public/\" + currentPath);|g" ./templates/pages/welcome_screen.html
+sed -i "s|window.open(baseURLBE + currentPath, \"_blank\");|window.open(baseURLBE + \"/public/\" + currentPath, \"_blank\");|g" ./templates/pages/welcome_screen.html
 
 end=$(date +%s)
 
