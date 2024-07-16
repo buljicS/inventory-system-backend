4359eba7bb2119d17aff38b87fe6055357a5d38e
stefan <stefanbvts@gmail.com>
2024-07-16 20:08:58 +0200

    swagger-php bumped to 4.10

--- bin/build.sh
+++ bin/build.sh
@@ -18,10 +18,10 @@ sed -i "s/^${ENV_DB_USER}=.*/${ENV_DB_USER}='$(printf '%s\n' "$NEW_DB_USER" | se
 sed -i "s/^${ENV_DB_PASSWD}=.*/${ENV_DB_PASSWD}='$(printf '%s\n' "$NEW_DB_PASSWD" | sed 's/[\/&]/\\&/g')'/" ./.env
 sed -i "s/^${ENV_URL_FE}=.*/${ENV_URL_FE}='$(printf '%s\n' "$NEW_URL_FE" | sed 's/[\/&]/\\&/g')'/" ./.env
 sed -i "s/^${ENV_URL_BE}=.*/${ENV_URL_BE}='$(printf '%s\n' "$NEW_URL_BE" | sed 's/[\/&]/\\&/g')'/" ./.env
-sed -i "s|let baseURLBE = \"http://www.insystem-api.localhost\";|let baseURLBE = \"${NEW_URL_BE}\";|g" ./templates/pages/welcome_screen.html
-sed -i "s|let URL_FE = \"https://localhost:3000\";|let URL_FE = \"${NEW_URL_FE}\";|g" ./templates/pages/welcome_screen.html
-sed -i "s|iFrame.setAttribute(\"src\", baseURLBE + currentPath);|iFrame.setAttribute(\"src\", baseURLBE + \"/public/\" + currentPath);|g" ./templates/pages/welcome_screen.html
-sed -i "s|window.open(baseURLBE + currentPath, \"_blank\");|window.open(baseURLBE + \"/public/\" + currentPath, \"_blank\");|g" ./templates/pages/welcome_screen.html
+#sed -i "s|let baseURLBE = \"http://www.insystem-api.localhost\";|let baseURLBE = \"${NEW_URL_BE}\";|g" ./templates/pages/welcome_screen.html
+#sed -i "s|let URL_FE = \"https://localhost:3000\";|let URL_FE = \"${NEW_URL_FE}\";|g" ./templates/pages/welcome_screen.html
+#sed -i "s|iFrame.setAttribute(\"src\", baseURLBE + currentPath);|iFrame.setAttribute(\"src\", baseURLBE + \"/public/\" + currentPath);|g" ./templates/pages/welcome_screen.html
+#sed -i "s|window.open(baseURLBE + currentPath, \"_blank\");|window.open(baseURLBE + \"/public/\" + currentPath, \"_blank\");|g" ./templates/pages/welcome_screen.html
 
 end=$(date +%s)
 
