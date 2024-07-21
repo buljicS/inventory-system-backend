a3b47ce6fe46ab4d1e285e521eb6648c9c30c84d
stefan <stefanbvts@gmail.com>
2024-07-21 19:46:06 +0200

    *

--- src/Repositories/FirebaseRepository.php
+++ src/Repositories/FirebaseRepository.php
@@ -19,7 +19,7 @@ class FirebaseRepository
 		$sql = "INSERT INTO pictures (picture_type_id, picture_name, picture_path, mime_type) VALUES (:picture_type_id, :picture_name, :picture_path, :mime_type)";
 		$stmt = $dbConn->prepare($sql);
 		$stmt->bindParam(':picture_type_id', $imageProps["picture_type"]);
-		$stmt->bindParam(':picture_name', $imageProps["name"]);
+		$stmt->bindParam(':picture_name', $imageProps["encoded_name"]);
 		$stmt->bindParam(':picture_path', $imageProps["picture_path"]);
 		$stmt->bindParam(':mime_type', $imageProps["type"]);
 		$stmt->execute();
