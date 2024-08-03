ce65f616d8c29601c74b2716b8036891c4cc0529
stefan <stefanbvts@gmail.com>
2024-08-03 23:31:33 +0200

    hotfix

--- src/Repositories/TaksRepository.php
+++ src/Repositories/TaksRepository.php
@@ -128,7 +128,7 @@ class TaksRepository
 		$stmt->bindParam(':task_id', $taskResponse['task_id']);
 		$stmt->bindParam(':summary', $taskResponse['task_summary']);
 		$stmt->bindParam(':status', $taskResponse['status']);
-		$end_date = new \DateTime('now');
+		$end_date = date('Y-m-d H:i:s', time());
 		$stmt->bindParam(':end_date', $end_date);
 		return $stmt->execute();
 	}
