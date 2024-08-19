3d4215ff86d166b33fe392908972872337011e5b
stefan <stefanbvts@gmail.com>
2024-08-19 15:50:13 +0200

    user can no longer create teams with same names

--- src/Repositories/TeamsRepository.php
+++ src/Repositories/TeamsRepository.php
@@ -76,6 +76,13 @@ class TeamsRepository
 	public function createNewTeam(array $newTeam): array
 	{
 		$notes = [];
+		$allTeams = $this->getAllTeams($newTeam['company_id']);
+		if(in_array($newTeam['team_name'], $allTeams))
+			return [
+				'status' => 401,
+				'message' => 'Forbidden',
+				'description' => 'Team with this name already exists'
+			];
 		$dbConn = $this->dbController->openConnection();
 		$sql = "INSERT INTO teams (team_name, worker_id, company_id) VALUES (:team_name, :worker_id, :company_id)";
 		$stmt = $dbConn->prepare($sql);
